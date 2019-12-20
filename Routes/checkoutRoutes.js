27a2cc3b2000316a0ec0da2f3a1f33014b408caf
Camilo Dominguez <camiloa17@gmail.com>
2019-12-20 12:53:17 -0500

    Se borro codigo comentado que estaba obsoleto y se arreglo paso1 para tener objeto de informacion de pagina

--- Routes/checkoutRoutes.js
+++ Routes/checkoutRoutes.js
@@ -7,11 +7,27 @@ const controller = require('../controllers/controller');
 
 router.get('/step_1/:materia/:tipo/:nivel/:modalidad', async (req, res) => {
     try {
-        const stylesheet = '/css/Front/checkoutStyle_Step1.css';
         const datosExamenModalidad = await controller.consultaExamenCheckout(req.query.id);
-        res.render('checkoutStep1', { stylesheet: stylesheet, nivel: datosExamenModalidad.nivel, modo: {texto:datosExamenModalidad.modalidad,exrw:datosExamenModalidad.exrw,exls:datosExamenModalidad.exls}, precio: datosExamenModalidad.precio, descripcion: datosExamenModalidad.descripcion, step: 'step_1', materia: datosExamenModalidad.materia, id: datosExamenModalidad.id, tipo: datosExamenModalidad.tipo });
+        const informacionPagina = {
+            stylesheet: '/css/Front/checkoutStyle_Step1.css',
+            step: 'step_1',
+            materia: datosExamenModalidad.materia,
+            tipo: datosExamenModalidad.tipo,
+            nivel: datosExamenModalidad.nivel,
+            modo: datosExamenModalidad.modalidad,
+            exrw: datosExamenModalidad.exrw,
+            exls: datosExamenModalidad.exls,
+            id: datosExamenModalidad.id,
+            horarioId: req.query.idhorario,
+            horarioLs: req.query.idhorarioL,
+            idReserva: req.query.idreserva,
+            precio: datosExamenModalidad.precio,
+            descripcion:datosExamenModalidad.descripcion
+        }
+
+        res.render('checkoutStep1', { stylesheet: informacionPagina.stylesheet, materia: informacionPagina.materia, tipo: informacionPagina.tipo, nivel: informacionPagina.nivel, modo: {texto:informacionPagina.modo,exrw:informacionPagina.exrw,exls:informacionPagina.exls}, precio: informacionPagina.precio, descripcion: informacionPagina.descripcion, step: informacionPagina.step, id: informacionPagina.id });
     } catch (err) {
-        console.error(err)
+        console.error(err,informacionPagina)
     }
 });
 
@@ -33,7 +49,7 @@ router.get('/step_2/:materia/:tipo/:nivel/:modalidad', async (req, res) => {
         
         res.render('checkoutStep2', { stylesheet: informacionPagina.stylesheet, materia: informacionPagina.materia, tipo: informacionPagina.tipo, nivel: informacionPagina.nivel, modo: { texto:informacionPagina.modo , exrw: informacionPagina.exrw, exls: informacionPagina.exls }, step: informacionPagina.step, id: informacionPagina, horario: horarios });
     } catch (err) {
-        console.error(err)
+        console.error(err,informacionPagina)
     }
 });
 
@@ -64,7 +80,7 @@ router.get('/step_3/:materia/:tipo/:nivel/:modalidad', async (req, res) => {
         }
 
     } catch (err) {
-        console.error(err);
+        console.error(err,informacionPagina);
     }
 });
 
@@ -97,7 +113,7 @@ router.get('/step_4/:materia/:tipo/:nivel/:modalidad', async (req, res) => {
         }
 
     } catch (err) {
-        console.error(err)
+        console.error(err,informacionPagina)
     }
 })
 
@@ -134,7 +150,7 @@ router.post('/horario-selected/:materia/:tipo/:nivel/:modalidad', async (req, re
 
         }
     } catch (err) {
-        console.log('error')
+        console.log(err,informacionPagina)
 
     }
 
@@ -142,77 +158,22 @@ router.post('/horario-selected/:materia/:tipo/:nivel/:modalidad', async (req, re
 
 
 router.get('/ver-fecha-fuera-de-termino/:modalidad',async(req,res)=>{
+    try{
         const datosExamenModalidad = await controller.consultaExamenCheckout(req.params.modalidad);
-        if(datosExamenModalidad.exrw===1 && datosExamenModalidad.exls===1){
-            const verFueraTerminoCo = await controller.verFechaFueraDeTermino({exrw:datosExamenModalidad.exrw,exls:datosExamenModalidad.exls}, req.query.horario, req.query.idhorarioL);
-            res.json({modalidad:{exrw:datosExamenModalidad.exrw,exls:datosExamenModalidad.exls},rw:verFueraTerminoCo.fecha_RW,ls:verFueraTerminoCo.fecha_ls});
-        }else{
+        if (datosExamenModalidad.exrw === 1 && datosExamenModalidad.exls === 1) {
+            const verFueraTerminoCo = await controller.verFechaFueraDeTermino({ exrw: datosExamenModalidad.exrw, exls: datosExamenModalidad.exls }, req.query.horario, req.query.idhorarioL);
+            res.json({ modalidad: { exrw: datosExamenModalidad.exrw, exls: datosExamenModalidad.exls }, rw: verFueraTerminoCo.fecha_RW, ls: verFueraTerminoCo.fecha_ls });
+        } else {
             const verFueraTermino = await controller.verFechaFueraDeTermino({ exrw: datosExamenModalidad.exrw, exls: datosExamenModalidad.exls }, req.query.horario);
-            res.json({ modalidad: { exrw: datosExamenModalidad.exrw, exls: datosExamenModalidad.exls }, rw:verFueraTermino.fecha})
+            res.json({ modalidad: { exrw: datosExamenModalidad.exrw, exls: datosExamenModalidad.exls }, rw: verFueraTermino.fecha })
         }
+    }catch(err){
+        console.error(err)
+    }
+        
      
 })
 
-//Consultar Con Juani si se hace esto
-/*
-
-router.get('/ver-horario/:idreserva', async (req, res) => {
-    try {
-        const consultarReserva = await controller.verReservarPaso3(req.params.idreserva);
-        res.json(consultarReserva[0])
-    } catch (err) {
-        console.log(err)
-    }
-});
-
-
-
-router.post('/horario-updated/:materia/:tipo/:nivel/:modalidad', async (req, res) => {
-    try {
-        
-        const datosExamenModalidad = await controller.consultaExamenCheckout(req.query.id);
-        const examen = {
-            materia: datosExamenModalidad.materia,
-            tipo: datosExamenModalidad.tipo,
-            nivel: datosExamenModalidad.nivel,
-            modalidad: datosExamenModalidad.modalidad
-        }
-        //Parte Compelto
-        if (examen.modalidad === 'Completo') {
-            //Corroboramos si existio un cambio de horario y si esta en los 10 minutos, si es asi se actualiza la reserva y se hace la redirección con
-            const corroborarCambioDeHorarioCompleto = await controller.corroborarCambioDeHorario(examen.modalidad, req.query.idreserva, req.query.idhorario, req.query.idhorarioL);
-            if(corroborarCambioDeHorarioCompleto){
-            const cambioReservaTemporalCompleto = await controller.actualizarReservaEnProceso(examen.modalidad, req.query.idhorario, req.query.idreserva,actualizarReservaEnProceso);
-            if (!cambioReservaTemporalCompleto) {
-                res.sendStatus(404);
-            } else {
-                res.redirect(`/checkout/step_3/${req.params.materia}/${req.params.tipo}/${req.params.nivel}/${req.params.modalidad}?id=${req.query.id}&idhorario=${req.query.idhorario}&idhorarioL=${req.query.idhorarioL}&idreserva=${req.query.idreserva}`);
-            }
-        }else{
-                res.redirect(`/checkout/step_3/${req.params.materia}/${req.params.tipo}/${req.params.nivel}/${req.params.modalidad}?id=${req.query.id}&idhorario=${req.query.idhorario}&idhorarioL=${req.query.idhorarioL}&idreserva=${req.query.idreserva}`);
-        }
-
-        // Parte RW
-        } else {
-            const corroborarCambioDeHorarioRwLs = await controller.corroborarCambioDeHorario(req.query.idreserva, req.query.idhorario);
-            if (corroborarCambioDeHorarioRwLs) {
-                const cambioReservaTemporalRwLs = await controller.actualizarReservaEnProceso(examen.modalidad, req.query.idhorario, req.query.idreserva);
-                if (!cambioReservaTemporalRwLs) {
-                    res.sendStatus(404);
-                } else {
-                    res.redirect(`/checkout/step_3/${req.params.materia}/${req.params.tipo}/${req.params.nivel}/${req.params.modalidad}?id=${req.query.id}&idhorario=${req.query.idhorario}&idreserva=${req.query.idreserva}`);
-                }
-            } else {
-                res.redirect(`/checkout/step_3/${req.params.materia}/${req.params.tipo}/${req.params.nivel}/${req.params.modalidad}?id=${req.query.id}&idhorario=${req.query.idhorario}&idreserva=${req.query.idreserva}`);
-            }
-        }
-
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-*/
 
 
 module.exports = router;
